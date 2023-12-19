49738ef7cea2e07934166103b5f15f3a1c5bd085
JanWilczek <janekw@vp.pl>
2023-12-19 12:22:17 +0100

    Make Neotree the default file explorer.

--- lua/jawi/plugins/neo-tree.lua
+++ lua/jawi/plugins/neo-tree.lua
@@ -7,7 +7,18 @@ return {
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+    },
+    config = function ()
+      require("neo-tree").setup({
+	default_component_configs = {
+	  window = {
+	    filesystem = {
+	      hijack_netrw_behavior = "open_current",
+	    },
+	  },
+	},
+      })
+    end
 }
 }
 
