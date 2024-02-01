1c4c78d21782098a96f6412a0724fee1c08f2210
JanWilczek <janekw@vp.pl>
2024-02-01 16:17:04 +0100

    Added more useful system clipboard shortcuts.

--- lua/jawi/init.lua
+++ lua/jawi/init.lua
@@ -271,6 +271,12 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Copy to and paste from system clipboard more easily
+vim.keymap.set({'n', 'v'}, '<leader>y', '"*y')
+vim.kepmap.set({'n', 'v'}, '<leader>p', '"*p')
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y')
+vim.kepmap.set({'n', 'v'}, '<leader>p', '"+p')
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
