6e9184d2e981d1952e2897489abe2662bff50f9b
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-03-18 08:05:38 +0100

    Temporarily disabled auto-session and made neo-tree non-lazy

--- lua/jawi/plugins/auto-session.lua
+++ lua/jawi/plugins/auto-session.lua
@@ -1,11 +1,19 @@
 return {
   "rmagatti/auto-session",
+  enabled = false,
   dependencies = {
     "nvim-telescope/telescope.nvim"
   },
   config = function()
     require("auto-session").setup {
-      log_level = "error"
+      log_level = "error",
+      -- auto_save_enabled = true,
+      -- auto_session_enabled = true,
+      auto_restore_enabled = false,
+      auto_session_use_git_branch = true,
+      auto_session_create_enabled = false,
+      auto_session_enable_last_session = false,
+      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
     }
   end
 }
--- lua/jawi/plugins/neo-tree.lua
+++ lua/jawi/plugins/neo-tree.lua
@@ -2,6 +2,7 @@ return {
         {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
@@ -31,7 +32,7 @@ return {
     config = function ()
       require("neo-tree").setup({
         close_if_last_window = true,
-        auto_clean_after_session_restore = true,
+        -- auto_clean_after_session_restore = false,
         default_component_configs = {
           window = {
             filesystem = {
