5e9f66da442b07f545b382fd0f5db44f8a4232ee
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-04-09 08:28:59 +0200

    Quick keymap fix.

--- lua/jawi/plugins/neovim-session-manager.lua
+++ lua/jawi/plugins/neovim-session-manager.lua
@@ -2,8 +2,8 @@ return {
   'Shatur/neovim-session-manager',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { "n", "<F5>", "<cmd>SessionManager save_current_session<CR>", desc = "Quick save current session" },
-    { "n", "<F9>", "<cmd>SessionManager load_current_dir_session<CR>", desc = "Quick load current dir session" },
+    { "<F5>", "<cmd>SessionManager save_current_session<CR>", desc = "Quick save current session" },
+    { "<F9>", "<cmd>SessionManager load_current_dir_session<CR>", desc = "Quick load current dir session" },
   },
   config = function()
     require 'session_manager'.setup({
