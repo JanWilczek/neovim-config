466947ee0582f1a5ea265c67656876cda86e13d7
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-05-11 08:33:44 +0200

    Changed replace behavior of the autocompletion plugin

--- lua/jawi/init.lua
+++ lua/jawi/init.lua
@@ -641,7 +641,7 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
     ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
+      behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     },
     ['<Tab>'] = cmp.mapping(function(fallback)
