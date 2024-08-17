28ea4ee1f63df24c91dfdfa321c06326ca86d5bd
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-08-17 11:44:06 +0200

    Highlight current line

--- lua/jawi/init.lua
+++ lua/jawi/init.lua
@@ -237,6 +237,7 @@ vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
+vim.o.cursorline = true
 
 -- Set listchars: Display tabs and trailing spaces visually
 vim.opt.listchars = {tab = '→ ', trail = '·', space = '·'}
