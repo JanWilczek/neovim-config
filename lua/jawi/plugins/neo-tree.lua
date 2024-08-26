60da3a91e9f646168d3feb9636358859d0b92db0
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-08-26 09:45:43 +0200

    Added a function and formatted file

--- lua/jawi/plugins/neo-tree.lua
+++ lua/jawi/plugins/neo-tree.lua
@@ -1,5 +1,45 @@
+local function copy_path(state)
+  -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+  -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+  local node = state.tree:get_node()
+  local filepath = node:get_id()
+  local filename = node.name
+  local modify = vim.fn.fnamemodify
+
+  local results = {
+    filepath,
+    modify(filepath, ":."),
+    modify(filepath, ":~"),
+    filename,
+    modify(filename, ":r"),
+    modify(filename, ":e"),
+  }
+
+  vim.ui.select({
+    "1. Absolute path: " .. results[1],
+    "2. Path relative to CWD: " .. results[2],
+    "3. Path relative to HOME: " .. results[3],
+    "4. Filename: " .. results[4],
+    "5. Filename without extension: " .. results[5],
+    "6. Extension of the filename: " .. results[6],
+  }, { prompt = "Choose to copy to clipboard:" }, function(choice)
+    if choice then
+      local i = tonumber(choice:sub(1, 1))
+      if i then
+        local result = results[i]
+        vim.fn.setreg('"', result)
+        vim.notify("Copied: " .. result)
+      else
+        vim.notify("Invalid selection")
+      end
+    else
+      vim.notify("Selection cancelled")
+    end
+  end)
+end
+
 return {
-        {
+  {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     lazy = false,
@@ -12,24 +52,24 @@ return {
         's1n7ax/nvim-window-picker',
         version = '2.*',
         config = function()
-            require 'window-picker'.setup({
-                hint = 'floating-big-letter',
-                filter_rules = {
-                    include_current_win = false,
-                    autoselect_one = true,
-                    -- filter using buffer options
-                    bo = {
-                        -- if the file type is one of following, the window will be ignored
-                        filetype = { 'neo-tree', "neo-tree-popup", "notify" },
-                        -- if the buffer type is one of following, the window will be ignored
-                        buftype = { 'terminal', "quickfix" },
-                    },
+          require 'window-picker'.setup({
+            hint = 'floating-big-letter',
+            filter_rules = {
+              include_current_win = false,
+              autoselect_one = true,
+              -- filter using buffer options
+              bo = {
+                -- if the file type is one of following, the window will be ignored
+                filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+                -- if the buffer type is one of following, the window will be ignored
+                buftype = { 'terminal', "quickfix" },
+              },
             },
-        })
+          })
         end,
       },
     },
-    config = function ()
+    config = function()
       require("neo-tree").setup({
         close_if_last_window = true,
         auto_clean_after_session_restore = false,
@@ -50,9 +90,8 @@ return {
       })
     end,
     keys = {
-      { "|", "<cmd>Neotree toggle left reveal_force_cwd<cr>", desc = "Neotree toggle file hierarchy"},
-      { "\\", "<cmd>Neotree reveal<cr>", desc = "Neotree reveal current file in hierarchy"},
+      { "|",  "<cmd>Neotree toggle left reveal_force_cwd<cr>", desc = "Neotree toggle file hierarchy" },
+      { "\\", "<cmd>Neotree reveal<cr>",                       desc = "Neotree reveal current file in hierarchy" },
     }
+  }
 }
-}
-
