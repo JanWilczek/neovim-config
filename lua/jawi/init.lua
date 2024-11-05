78fb05a974131862e543f50c46d45cbb252db8db
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-11-05 18:35:17 +0100

    Ensure Markdown treesitter is installed

--- lua/jawi/init.lua
+++ lua/jawi/init.lua
@@ -492,7 +492,7 @@ vim.defer_fn(function()
   require 'nvim-treesitter.install'.compilers = { "clang" }
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'cmake' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'cmake', 'markdown' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
