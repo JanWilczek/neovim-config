31c30f023d2e300027532f89c243a1d21bddc214
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-11-27 08:49:15 +0100

    Remove explicit servers from Mason
    I needed to fix a clangd issue and Mason was just not up to date

--- lua/jawi/init.lua
+++ lua/jawi/init.lua
@@ -643,8 +643,6 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  clangd = require("jawi.lsp.config.clangd"),
-  -- gopls = {},
   pyright = {},
   rust_analyzer = {},
   eslint = {},
