cd3c153c9532220aa4d1ca00fe0ec5c3829fbb8c
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2024-12-02 08:43:49 +0100

    Make DAP keybindings same as VS Code debugger

--- lua/jawi/plugins/neovim-session-manager.lua
+++ lua/jawi/plugins/neovim-session-manager.lua
@@ -2,8 +2,8 @@ return {
   'Shatur/neovim-session-manager',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { "<F5>", "<cmd>SessionManager save_current_session<CR>", desc = "Quick save current session" },
-    { "<F9>", "<cmd>SessionManager load_current_dir_session<CR>", desc = "Quick load current dir session" },
+    { "<leader>ss", "<cmd>SessionManager save_current_session<CR>", desc = "Quick save current session" },
+    { "<leader>ls", "<cmd>SessionManager load_current_dir_session<CR>", desc = "Quick load current dir session" },
   },
   config = function()
     require 'session_manager'.setup({
