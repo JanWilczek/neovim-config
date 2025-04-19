c51c9e554855ae411cf5d49b815584ce256ef144
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-04-19 08:26:09 +0200

    Increased auto-session log level

--- lua/jawi/plugins/auto-session.lua
+++ lua/jawi/plugins/auto-session.lua
@@ -6,7 +6,7 @@ return {
   },
   config = function()
     require("auto-session").setup {
-      log_level = "info",
+      log_level = "warning",
       auto_save_enabled = true,
       auto_session_enabled = true,
       auto_restore_enabled = true,
