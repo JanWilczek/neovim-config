686a72f4b1a10199f62779ee356449eec7cf0b11
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2025-07-11 07:43:03 +0200

    Removed 4 spaces indent from C++ files

--- lua/jawi/cpp/init.lua
+++ lua/jawi/cpp/init.lua
@@ -48,8 +48,8 @@ vim.api.nvim_set_keymap('n', '<leader>cx', ':lua CreateCppAndSplitOpen()<CR>',
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "cpp",
   callback = function()
-    vim.opt_local.tabstop = 4
-    vim.opt_local.softtabstop = 4
-    vim.opt_local.shiftwidth = 4
+    -- vim.opt_local.tabstop = 4
+    -- vim.opt_local.softtabstop = 4
+    -- vim.opt_local.shiftwidth = 4
   end
 })
