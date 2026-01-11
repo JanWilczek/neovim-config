ec73f5b1b996ecac8177d26feaf3f58a0b594344
Jan Wilczek <24981875+JanWilczek@users.noreply.github.com>
2026-01-11 09:56:53 +0100

    Fixed indent in Markdown files

--- lua/jawi/init.lua
+++ lua/jawi/init.lua
@@ -204,9 +204,9 @@ require('lazy').setup({
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        additional_vim_regex_highlighting = { 'ruby', 'markdown' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = { 'ruby', 'markdown' } },
     },
   },
 
